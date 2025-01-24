fd9ff19048f92e7f497656662e5495f3a1b2003c
Simran Sachdeva <simransachdeva789@gmail.com>
2025-01-24 23:01:49 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = 'AIzaSyAyfXEA92eRwrYlOy07DTyScc_Vpy6XZnI';
+const GEMINI_API_KEY = '';
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
 
 let currentHintIndex = 0;
